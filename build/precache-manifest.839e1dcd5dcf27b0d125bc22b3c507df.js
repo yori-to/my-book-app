e9fcd00c95d58c3e68f4f0a47a6ac1b39356e92f
ypetkovarado <ypetkovarado@dxc.com>
2019-09-13 11:57:39 +0300

    Create precache-manifest.839e1dcd5dcf27b0d125bc22b3c507df.js

---
+++ build/precache-manifest.839e1dcd5dcf27b0d125bc22b3c507df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70c43323ffcca455ea507e7c280677e",
+    "url": "/my-book-app/index.html"
+  },
+  {
+    "revision": "58d9f620a78c4690daef",
+    "url": "/my-book-app/static/css/main.24de87c2.chunk.css"
+  },
+  {
+    "revision": "e1f551fb1aaa94fd2d7b",
+    "url": "/my-book-app/static/js/2.fcebc60c.chunk.js"
+  },
+  {
+    "revision": "58d9f620a78c4690daef",
+    "url": "/my-book-app/static/js/main.bd63061f.chunk.js"
+  },
+  {
+    "revision": "47eb8dd3dc070717455f",
+    "url": "/my-book-app/static/js/runtime~main.1923a628.js"
+  }
+]);
